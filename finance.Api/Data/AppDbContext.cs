3c70766ab549bf6a40384a573152596c0fc7f242
Felipe Costa <felipecostasiqu@gmail.com>
2025-07-11 11:11:45 -0300

    ApiModels

---
+++ finance.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using finance.Core.Models;
+
+namespace finance.Api.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; }  = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetCallingAssembly());
+}
